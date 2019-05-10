bfcabeef0ba4dd1cfd16205aecce10ec00a9a344
adf694g <alwyn.burger@uni-due.de>
2019-05-10 17:04:07 +0200

    build script fixing

--- buildAll.sh
+++ buildAll.sh
@@ -4,7 +4,7 @@ function build_and_zip {
     bazel build :pkg --config=$2
     if [ ! -z "$OUTPUT_DIR" ]; then
         mkdir -p "$OUTPUT_DIR"/"$1"/
-        cp -f bazel-bin/"$1".zip "$OUTPUT_DIR"/"$1"/"$1"-"$2".zip
+        cp -f bazel-bin/pkg.tar.gz "$OUTPUT_DIR"/"$1"/"$1"-"$2".tar.gz
     fi
 }
 
